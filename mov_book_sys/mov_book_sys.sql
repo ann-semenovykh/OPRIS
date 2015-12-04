7446ca19d5a408680e7a41d240e47fe947d485a1
ann-semenovykh <anyuta12_15@list.ru>
2015-12-04 22:40:12 +0500

    Изменена структура БД
    Добавлена новая таблица с бронированными местами, изменена структура
    связанных таблиц, добавлены данные

--- mov_book_sys/mov_book_sys.sql
+++ mov_book_sys/mov_book_sys.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Дек 03 2015 г., 22:16
+-- Время создания: Дек 04 2015 г., 20:37
 -- Версия сервера: 5.5.45
 -- Версия PHP: 5.3.29
 
@@ -23,6 +23,192 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `booking_seats`
+--
+
+CREATE TABLE IF NOT EXISTS `booking_seats` (
+  `id_session` int(11) DEFAULT NULL,
+  `id_seat` int(11) DEFAULT NULL,
+  `stat` enum('free','reserved','ordered') DEFAULT 'free',
+  `id_user` int(11) DEFAULT NULL,
+  `reserve_time` datetime DEFAULT NULL,
+  KEY `FK_booking_seats_session` (`id_session`),
+  KEY `FK_booking_seats_seats` (`id_seat`),
+  KEY `FK_booking_seats_users` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Зарезервированные и свободные места';
+
+--
+-- Дамп данных таблицы `booking_seats`
+--
+
+INSERT INTO `booking_seats` (`id_session`, `id_seat`, `stat`, `id_user`, `reserve_time`) VALUES
+(1, 1, 'free', NULL, NULL),
+(1, 2, 'free', NULL, NULL),
+(1, 3, 'free', NULL, NULL),
+(1, 4, 'free', NULL, NULL),
+(1, 5, 'free', NULL, NULL),
+(1, 6, 'free', NULL, NULL),
+(1, 7, 'free', NULL, NULL),
+(1, 8, 'free', NULL, NULL),
+(1, 9, 'free', NULL, NULL),
+(1, 10, 'free', NULL, NULL),
+(1, 11, 'free', NULL, NULL),
+(1, 12, 'free', NULL, NULL),
+(1, 13, 'free', NULL, NULL),
+(1, 14, 'free', NULL, NULL),
+(1, 15, 'free', NULL, NULL),
+(1, 16, 'free', NULL, NULL),
+(1, 17, 'free', NULL, NULL),
+(1, 18, 'free', NULL, NULL),
+(1, 19, 'free', NULL, NULL),
+(1, 20, 'free', NULL, NULL),
+(1, 21, 'free', NULL, NULL),
+(1, 22, 'free', NULL, NULL),
+(2, 1, 'free', NULL, NULL),
+(2, 2, 'free', NULL, NULL),
+(2, 3, 'free', NULL, NULL),
+(2, 4, 'free', NULL, NULL),
+(2, 5, 'free', NULL, NULL),
+(2, 6, 'free', NULL, NULL),
+(2, 7, 'free', NULL, NULL),
+(2, 8, 'free', NULL, NULL),
+(2, 9, 'free', NULL, NULL),
+(2, 10, 'free', NULL, NULL),
+(2, 11, 'free', NULL, NULL),
+(2, 12, 'free', NULL, NULL),
+(2, 13, 'free', NULL, NULL),
+(2, 14, 'free', NULL, NULL),
+(2, 15, 'free', NULL, NULL),
+(2, 16, 'free', NULL, NULL),
+(2, 17, 'free', NULL, NULL),
+(2, 18, 'free', NULL, NULL),
+(2, 19, 'free', NULL, NULL),
+(2, 20, 'free', NULL, NULL),
+(2, 21, 'free', NULL, NULL),
+(2, 22, 'free', NULL, NULL),
+(3, 1, 'free', NULL, NULL),
+(3, 2, 'free', NULL, NULL),
+(3, 3, 'free', NULL, NULL),
+(3, 4, 'free', NULL, NULL),
+(3, 5, 'free', NULL, NULL),
+(3, 6, 'free', NULL, NULL),
+(3, 7, 'free', NULL, NULL),
+(3, 8, 'free', NULL, NULL),
+(3, 9, 'free', NULL, NULL),
+(3, 10, 'free', NULL, NULL),
+(3, 11, 'free', NULL, NULL),
+(3, 12, 'free', NULL, NULL),
+(3, 13, 'free', NULL, NULL),
+(3, 14, 'free', NULL, NULL),
+(3, 15, 'free', NULL, NULL),
+(3, 16, 'free', NULL, NULL),
+(3, 17, 'free', NULL, NULL),
+(3, 18, 'free', NULL, NULL),
+(3, 19, 'free', NULL, NULL),
+(3, 20, 'free', NULL, NULL),
+(3, 21, 'free', NULL, NULL),
+(3, 22, 'free', NULL, NULL),
+(6, 1, 'free', NULL, NULL),
+(6, 2, 'free', NULL, NULL),
+(6, 3, 'free', NULL, NULL),
+(6, 4, 'free', NULL, NULL),
+(6, 5, 'free', NULL, NULL),
+(6, 6, 'free', NULL, NULL),
+(6, 7, 'free', NULL, NULL),
+(6, 8, 'free', NULL, NULL),
+(6, 9, 'free', NULL, NULL),
+(6, 10, 'free', NULL, NULL),
+(6, 11, 'free', NULL, NULL),
+(6, 12, 'free', NULL, NULL),
+(6, 13, 'free', NULL, NULL),
+(6, 14, 'free', NULL, NULL),
+(6, 15, 'free', NULL, NULL),
+(6, 16, 'free', NULL, NULL),
+(6, 17, 'free', NULL, NULL),
+(6, 18, 'free', NULL, NULL),
+(6, 19, 'free', NULL, NULL),
+(6, 20, 'free', NULL, NULL),
+(6, 21, 'free', NULL, NULL),
+(6, 22, 'free', NULL, NULL),
+(4, 23, 'free', NULL, NULL),
+(4, 24, 'free', NULL, NULL),
+(4, 25, 'free', NULL, NULL),
+(4, 26, 'free', NULL, NULL),
+(4, 27, 'free', NULL, NULL),
+(4, 28, 'free', NULL, NULL),
+(4, 29, 'free', NULL, NULL),
+(4, 30, 'free', NULL, NULL),
+(4, 31, 'free', NULL, NULL),
+(4, 32, 'free', NULL, NULL),
+(4, 33, 'free', NULL, NULL),
+(4, 34, 'free', NULL, NULL),
+(4, 35, 'free', NULL, NULL),
+(4, 36, 'free', NULL, NULL),
+(4, 37, 'free', NULL, NULL),
+(4, 38, 'free', NULL, NULL),
+(5, 23, 'free', NULL, NULL),
+(5, 24, 'free', NULL, NULL),
+(5, 25, 'free', NULL, NULL),
+(5, 26, 'free', NULL, NULL),
+(5, 27, 'free', NULL, NULL),
+(5, 28, 'free', NULL, NULL),
+(5, 29, 'free', NULL, NULL),
+(5, 30, 'free', NULL, NULL),
+(5, 31, 'free', NULL, NULL),
+(5, 32, 'free', NULL, NULL),
+(5, 33, 'free', NULL, NULL),
+(5, 34, 'free', NULL, NULL),
+(5, 35, 'free', NULL, NULL),
+(5, 36, 'free', NULL, NULL),
+(5, 37, 'free', NULL, NULL),
+(5, 38, 'free', NULL, NULL),
+(9, 23, 'free', NULL, NULL),
+(9, 24, 'free', NULL, NULL),
+(9, 25, 'free', NULL, NULL),
+(9, 26, 'free', NULL, NULL),
+(9, 27, 'free', NULL, NULL),
+(9, 28, 'free', NULL, NULL),
+(9, 29, 'free', NULL, NULL),
+(9, 30, 'free', NULL, NULL),
+(9, 31, 'free', NULL, NULL),
+(9, 32, 'free', NULL, NULL),
+(9, 33, 'free', NULL, NULL),
+(9, 34, 'free', NULL, NULL),
+(9, 35, 'free', NULL, NULL),
+(9, 36, 'free', NULL, NULL),
+(9, 37, 'free', NULL, NULL),
+(9, 38, 'free', NULL, NULL),
+(7, 39, 'free', NULL, NULL),
+(8, 39, 'free', NULL, NULL),
+(10, 39, 'free', NULL, NULL),
+(7, 40, 'free', NULL, NULL),
+(7, 41, 'free', NULL, NULL),
+(7, 42, 'free', NULL, NULL),
+(7, 43, 'free', NULL, NULL),
+(7, 44, 'free', NULL, NULL),
+(7, 45, 'free', NULL, NULL),
+(7, 46, 'free', NULL, NULL),
+(7, 47, 'free', NULL, NULL),
+(8, 40, 'free', NULL, NULL),
+(8, 41, 'free', NULL, NULL),
+(8, 42, 'free', NULL, NULL),
+(8, 43, 'free', NULL, NULL),
+(8, 44, 'free', NULL, NULL),
+(8, 45, 'free', NULL, NULL),
+(8, 46, 'free', NULL, NULL),
+(8, 47, 'free', NULL, NULL),
+(10, 40, 'free', NULL, NULL),
+(10, 41, 'free', NULL, NULL),
+(10, 42, 'free', NULL, NULL),
+(10, 43, 'free', NULL, NULL),
+(10, 44, 'free', NULL, NULL),
+(10, 45, 'free', NULL, NULL),
+(10, 46, 'free', NULL, NULL),
+(10, 47, 'free', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `hall`
 --
 
@@ -30,6 +216,7 @@ CREATE TABLE IF NOT EXISTS `hall` (
   `id_hall` int(11) NOT NULL AUTO_INCREMENT,
   `stat` enum('VIP','малый','большой') NOT NULL,
   `name` char(20) DEFAULT NULL,
+  `descr` char(255) DEFAULT NULL,
   PRIMARY KEY (`id_hall`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Зал кинотеатра' AUTO_INCREMENT=5 ;
 
@@ -37,11 +224,10 @@ CREATE TABLE IF NOT EXISTS `hall` (
 -- Дамп данных таблицы `hall`
 --
 
-INSERT INTO `hall` (`id_hall`, `stat`, `name`) VALUES
-(1, 'большой', '1'),
-(2, 'малый', '2'),
-(3, 'большой', 'Синий'),
-(4, 'малый', 'Красный');
+INSERT INTO `hall` (`id_hall`, `stat`, `name`, `descr`) VALUES
+(1, 'большой', '1', '4 ряда по 4 места + 5 ряд 6 мест = 22 места'),
+(2, 'малый', '2', '4 ряда по 4 места = 16 мест'),
+(3, 'VIP', 'Черный', '3 ряда по 3 места = 9 мест');
 
 -- --------------------------------------------------------
 
@@ -99,27 +285,65 @@ CREATE TABLE IF NOT EXISTS `orders` (
 
 CREATE TABLE IF NOT EXISTS `seats` (
   `id_seat` int(11) NOT NULL AUTO_INCREMENT,
-  `numseries` int(11) NOT NULL,
+  `numseries` int(11) NOT NULL DEFAULT '0',
   `num` int(11) NOT NULL,
-  `stat` enum('free','reserved','ordered') NOT NULL,
-  `id_user` int(11) DEFAULT NULL,
-  `reserve_time` datetime DEFAULT NULL,
+  `id_hall` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_seat`),
-  KEY `FK_seats_users` (`id_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Места в кинотеатре' AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+  KEY `id_hall` (`id_hall`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Места в зале' AUTO_INCREMENT=48 ;
 
 --
--- Структура таблицы `seats_in_hall`
+-- Дамп данных таблицы `seats`
 --
 
-CREATE TABLE IF NOT EXISTS `seats_in_hall` (
-  `id_hall` int(11) DEFAULT NULL,
-  `id_seat` int(11) DEFAULT NULL,
-  KEY `FK__hall` (`id_hall`),
-  KEY `FK__seats` (`id_seat`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Места в зале';
+INSERT INTO `seats` (`id_seat`, `numseries`, `num`, `id_hall`) VALUES
+(1, 1, 2, 1),
+(2, 1, 1, 1),
+(3, 1, 3, 1),
+(4, 1, 4, 1),
+(5, 2, 1, 1),
+(6, 2, 2, 1),
+(7, 2, 3, 1),
+(8, 2, 4, 1),
+(9, 3, 4, 1),
+(10, 3, 3, 1),
+(11, 3, 2, 1),
+(12, 3, 1, 1),
+(13, 4, 1, 1),
+(14, 4, 2, 1),
+(15, 4, 3, 1),
+(16, 4, 4, 1),
+(17, 5, 4, 1),
+(18, 5, 3, 1),
+(19, 5, 2, 1),
+(20, 5, 1, 1),
+(21, 5, 5, 1),
+(22, 5, 6, 1),
+(23, 1, 1, 2),
+(24, 1, 2, 2),
+(25, 1, 3, 2),
+(26, 1, 4, 2),
+(27, 2, 4, 2),
+(28, 2, 3, 2),
+(29, 2, 4, 2),
+(30, 2, 1, 2),
+(31, 3, 1, 2),
+(32, 3, 2, 2),
+(33, 3, 3, 2),
+(34, 3, 4, 2),
+(35, 4, 1, 2),
+(36, 4, 2, 2),
+(37, 4, 3, 2),
+(38, 4, 4, 2),
+(39, 1, 1, 3),
+(40, 1, 2, 3),
+(41, 1, 3, 3),
+(42, 2, 3, 3),
+(43, 2, 2, 3),
+(44, 2, 1, 3),
+(45, 3, 1, 3),
+(46, 3, 2, 3),
+(47, 3, 3, 3);
 
 -- --------------------------------------------------------
 
@@ -128,29 +352,31 @@ CREATE TABLE IF NOT EXISTS `seats_in_hall` (
 --
 
 CREATE TABLE IF NOT EXISTS `session` (
+  `id_session` int(11) NOT NULL AUTO_INCREMENT,
   `id_mov` int(11) DEFAULT NULL,
   `id_hall` int(11) DEFAULT NULL,
   `time` datetime DEFAULT NULL,
   `price` float DEFAULT NULL,
-  KEY `FK_session_movies` (`id_mov`),
-  KEY `FK_session_hall` (`id_hall`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Сеанс фильма';
+  PRIMARY KEY (`id_session`),
+  KEY `FK_session_hall` (`id_hall`),
+  KEY `FK_session_movies` (`id_mov`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Сеанс фильма' AUTO_INCREMENT=11 ;
 
 --
 -- Дамп данных таблицы `session`
 --
 
-INSERT INTO `session` (`id_mov`, `id_hall`, `time`, `price`) VALUES
-(1, 1, '2015-12-02 22:45:00', 100),
-(1, 1, '2015-12-02 12:45:00', 100),
-(4, 1, '2015-12-02 22:45:00', 100),
-(4, 2, '2015-12-02 23:40:00', 100),
-(2, 2, '2015-12-03 22:30:00', 250),
-(1, 1, '2015-12-03 20:00:00', 150),
-(4, 4, '2015-12-03 20:00:00', 150),
-(4, 4, '2015-12-03 10:00:00', 120),
-(1, 2, '2015-12-03 20:00:00', 300),
-(2, 3, '2015-12-03 21:50:00', 150);
+INSERT INTO `session` (`id_session`, `id_mov`, `id_hall`, `time`, `price`) VALUES
+(1, 1, 1, '2015-12-02 22:45:00', 100),
+(2, 1, 1, '2015-12-02 12:45:00', 100),
+(3, 4, 1, '2015-12-02 22:45:00', 100),
+(4, 4, 2, '2015-12-02 23:40:00', 100),
+(5, 2, 2, '2015-12-03 22:30:00', 250),
+(6, 1, 1, '2015-12-03 20:00:00', 150),
+(7, 4, 3, '2015-12-03 20:00:00', 150),
+(8, 4, 3, '2015-12-03 10:00:00', 120),
+(9, 1, 2, '2015-12-03 20:00:00', 300),
+(10, 2, 3, '2015-12-03 21:50:00', 150);
 
 -- --------------------------------------------------------
 
@@ -186,6 +412,14 @@ INSERT INTO `users` (`id_user`, `name`, `surname`, `birthdate`, `account_name`, 
 --
 
 --
+-- Ограничения внешнего ключа таблицы `booking_seats`
+--
+ALTER TABLE `booking_seats`
+  ADD CONSTRAINT `FK_booking_seats_seats` FOREIGN KEY (`id_seat`) REFERENCES `seats` (`id_seat`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_booking_seats_session` FOREIGN KEY (`id_session`) REFERENCES `session` (`id_session`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_booking_seats_users` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
 -- Ограничения внешнего ключа таблицы `orders`
 --
 ALTER TABLE `orders`
@@ -196,21 +430,14 @@ ALTER TABLE `orders`
 -- Ограничения внешнего ключа таблицы `seats`
 --
 ALTER TABLE `seats`
-  ADD CONSTRAINT `FK_seats_users` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE SET NULL ON UPDATE CASCADE;
-
---
--- Ограничения внешнего ключа таблицы `seats_in_hall`
---
-ALTER TABLE `seats_in_hall`
-  ADD CONSTRAINT `FK__hall` FOREIGN KEY (`id_hall`) REFERENCES `hall` (`id_hall`),
-  ADD CONSTRAINT `FK__seats` FOREIGN KEY (`id_seat`) REFERENCES `seats` (`id_seat`);
+  ADD CONSTRAINT `FK_seats_hall` FOREIGN KEY (`id_hall`) REFERENCES `hall` (`id_hall`);
 
 --
 -- Ограничения внешнего ключа таблицы `session`
 --
 ALTER TABLE `session`
-  ADD CONSTRAINT `FK_session_hall` FOREIGN KEY (`id_hall`) REFERENCES `hall` (`id_hall`),
-  ADD CONSTRAINT `FK_session_movies` FOREIGN KEY (`id_mov`) REFERENCES `movies` (`id_mov`);
+  ADD CONSTRAINT `FK_session_hall` FOREIGN KEY (`id_hall`) REFERENCES `hall` (`id_hall`) ON DELETE SET NULL,
+  ADD CONSTRAINT `FK_session_movies` FOREIGN KEY (`id_mov`) REFERENCES `movies` (`id_mov`) ON DELETE SET NULL;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
