6e67a90324815297842a73db80c7b1b6634a94fc
ann-semenovykh <anyuta12_15@list.ru>
2015-12-30 19:14:54 +0500

    Верстка панели администратора
    Добавление/изменение сеансов

--- kinoteatr/application/views/admin_add_session.php
+++ kinoteatr/application/views/admin_add_session.php
@@ -7,32 +7,32 @@
 				</div>
 				<form name = "personInfo_form" action = "/admin/add_session" method = "post" enctype="multipart/form-data">
 					<div class = "personalInfo_field">
-						<h4 class = "personInfo_label">Сеанс</h4>
-						<select name = "movie_id">
+						<h4 class = "panelInfo_label">Сеанс</h4>
+						<select name = "movie_id" style="left:50px;	position:relative;	margin:4px;	top:12px;">
 							<?php foreach ($movies as $movie){?>
 							<option value = "<?php echo $movie->id_mov;?>"><?php echo $movie->name;}?></option>
 						</select>
 					</div>
 					<div class = "personalInfo_field">
-						<h4 class = "personInfo_label">Зал</h4>
-						<select name = "hall_id">
+						<h4 class = "panelInfo_label">Зал</h4>
+						<select name = "hall_id" style="left:50px;	position:relative;	margin:4px;	top:12px;">
 							<?php foreach ($halls as $hall){?>
 							<option value = "<?php echo $hall->id_hall;?>"><?php echo $hall->name;}?></option>
 						</select>
 					</div>
 					<div class = "personalInfo_field">
-						<h4 class = "personInfo_label">Время</h4>
-						<input type = "text" class = "personInfo_text" size="33" style="width:301px" value = "" name = "setDate">
+						<h4 class = "panelInfo_label">Время</h4>
+						<input type = "text" class = "panelInfo_text" size="33" style="width:301px" value = "" name = "setDate">
 					</div>
 					<div class = "personalInfo_field">
-						<h4 class = "personInfo_label">Цена</h4>
-						<input type = "text" class = "personInfo_text" size="33" style="width:301px" value = "" name = "price">
+						<h4 class = "panelInfo_label">Цена</h4>
+						<input type = "text" class = "panelInfo_text" size="33" style="width:301px" value = "" name = "price">
 					</div>
 					<?php
 						if (isset($errors)){
 							echo "<h4 class = \"personInfo_label\">$errors</h4>";
 						}
 					?>
-					<input type = "submit" name = "submit_new" class="personBtn" value = "Сохранить">
+					<input type = "submit" name = "submit_new" class="panelBtn" value = "Сохранить">
 				</form>
 </div>
